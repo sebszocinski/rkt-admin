81ad1c20ef63ebeca3c691b94c6b05cf9ddc691d
Seb Szocinski <seb@rockethouse.com.au>
2015-04-29 12:20:13 +0930

    Removed widgets on dashboard
    Including Mandrill which shouldn’t be there.

--- index.php
+++ index.php
@@ -90,6 +90,21 @@ function remove_admin_columns() {
 
 
 
+// Remove Widgets
+function remove_dashboard_meta() {
+        remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+        remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+        remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+        remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
+        remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+        remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+        remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+        remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+        remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
+        remove_meta_box( 'mandrill_widget', 'dashboard', 'normal');
+}
+add_action( 'admin_init', 'remove_dashboard_meta' );
+
 
 
 
